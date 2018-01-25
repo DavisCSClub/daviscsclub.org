554158905df88b6b9ba5b8fa639a67e1b0d43243
Corey Mason <corey.mason98@googlemail.com>
2018-01-25 00:47:21 -0800

    Remove i18n support
    If generators or other internal scripts break, this commit should be investigated due to the sheer number of related modifications to core files.

--- internals/generators/index.js
+++ internals/generators/index.js
@@ -8,12 +8,10 @@ const fs = require('fs');
 const path = require('path');
 const componentGenerator = require('./component/index.js');
 const containerGenerator = require('./container/index.js');
-const languageGenerator = require('./language/index.js');
 
 module.exports = (plop) => {
   plop.setGenerator('component', componentGenerator);
   plop.setGenerator('container', containerGenerator);
-  plop.setGenerator('language', languageGenerator);
   plop.addHelper('directory', (comp) => {
     try {
       fs.accessSync(path.join(__dirname, `../../app/containers/${comp}`), fs.F_OK);
--- internals/scripts/clean.js
+++ internals/scripts/clean.js
@@ -1,10 +1,12 @@
 const shell = require('shelljs');
 const addCheckMark = require('./helpers/checkmark.js');
 
-/*if (!shell.which('git')) {
+/*
+if (!shell.which('git')) {
   shell.echo('Sorry, this script requires git');
   shell.exit(1);
-}*/
+}
+*/
 
 if (!shell.test('-e', 'internals/templates')) {
   shell.echo('The example is deleted already.');
@@ -12,10 +14,6 @@ if (!shell.test('-e', 'internals/templates')) {
 }
 
 process.stdout.write('Cleanup started...');
-
-// Reuse existing LanguageProvider and i18n tests
-shell.mv('app/containers/LanguageProvider/tests', 'internals/templates/containers/LanguageProvider');
-shell.cp('app/tests/i18n.test.js', 'internals/templates/tests/i18n.test.js');
 
 // Cleanup components/
 shell.rm('-rf', 'app/components/*');
@@ -27,18 +25,13 @@ shell.mv('internals/templates/containers', 'app');
 // Handle tests/
 shell.mv('internals/templates/tests', 'app');
 
-// Handle translations/
-shell.rm('-rf', 'app/translations')
-shell.mv('internals/templates/translations', 'app');
-
 // Handle utils/
 shell.rm('-rf', 'app/utils');
-shell.mv('internals/templates/utils', 'app')
+shell.mv('internals/templates/utils', 'app');
 
 // Replace the files in the root app/ folder
 shell.cp('internals/templates/app.js', 'app/app.js');
 shell.cp('internals/templates/global-styles.js', 'app/global-styles.js');
-shell.cp('internals/templates/i18n.js', 'app/i18n.js');
 shell.cp('internals/templates/index.html', 'app/index.html');
 shell.cp('internals/templates/reducers.js', 'app/reducers.js');
 shell.cp('internals/templates/configureStore.js', 'app/configureStore.js');
@@ -49,9 +42,11 @@ shell.rm('-rf', 'internals/templates');
 addCheckMark();
 
 // Commit the changes
-/*if (shell.exec('git add . --all && git commit -qm "Remove default example"').code !== 0) {
+/*
+if (shell.exec('git add . --all && git commit -qm "Remove default example"').code !== 0) {
   shell.echo('\nError: Git commit failed');
   shell.exit(1);
-}*/
+}
+*/
 
 shell.echo('\nCleanup done. Happy Coding!!!');
