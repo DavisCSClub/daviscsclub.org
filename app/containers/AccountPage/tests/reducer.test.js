7bad31b6af34aef44135c98d118846890279baf9
Corey Mason <corey.mason98@googlemail.com>
2018-07-15 15:15:43 -0700

    Start Fresh

--- app/containers/AccountPage/tests/reducer.test.js
+++
@@ -1,9 +0,0 @@
-
-import { fromJS } from 'immutable';
-import accountPageReducer from '../reducer';
-
-describe('accountPageReducer', () => {
-  it('returns the initial state', () => {
-    expect(accountPageReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
--- app/containers/Auth/tests/reducer.test.js
+++
@@ -1,9 +0,0 @@
-
-import { fromJS } from 'immutable';
-import authReducer from '../reducer';
-
-describe('authReducer', () => {
-  it('returns the initial state', () => {
-    expect(authReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
