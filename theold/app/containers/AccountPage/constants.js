34c8ef2e4331f791b8a36ab6e60413616377d7c8
Corey Mason <corey.mason98@googlemail.com>
2018-09-03 16:15:38 -0700

    Out with the old

--- theold/app/containers/AccountPage/constants.js
+++
@@ -1,9 +0,0 @@
-/*
- *
- * AccountPage constants
- *
- */
-
- export const LOAD_USER_DATA = 'app/HomePage/LOAD_USER_DATA';
- export const LOAD_USER_DATA_SUCCESS = 'app/HomePage/LOAD_USER_DATA_SUCCESS';
- export const LOAD_USER_DATA_FAIL = 'app/HomePage/LOAD_USER_DATA_FAIL';
