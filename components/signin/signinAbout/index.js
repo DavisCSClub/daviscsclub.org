8964510046309358e006b59fd15a553f52655556
Shivam Desai <31487349+shivampdesai@users.noreply.github.com>
2019-02-18 21:19:55 -0800

    updated signin for week 7 winter quarter

--- components/signin/signinAbout/index.js
+++ components/signin/signinAbout/index.js
@@ -14,7 +14,7 @@ function signinAbout() {
         <div className="container">
           <div className="row">
             <div className="col-lg-7">
-              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf1pebn7VTQQEPOF5NPy1jDDWybN5jPlNfijz7hBdiLAf-JqQ/viewform?embedded=true" width="1076" height="1076" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf3BbsK0L1T4F0xONbcqvDKvdsPQWDoGaGtVkqKELo56AqZYA/viewform?embedded=true" width="1076" height="1076" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
             </div>
           </div>
         </div>
